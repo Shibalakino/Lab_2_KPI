e8469f1c3e47bd73f831f8b58ffeaa04b3e1152c
Oleg Boyko <57860598+Shibalakino@users.noreply.github.com>
2020-10-30 13:45:39 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+x = float(input("enter a value of point x: "))
+y = float(input("enter a value of point y: "))
+if(y >= x and y >= -x and y <= 1 - x * x / 1):
+    {
+print("your values are in range \n")
+    }
+else:
+    {
+        print("your values aren`t in range \n")
+    }
